06a69aa8f86c54299c90604feaeb4108330039de
Thea / Aesther <154717468+Josephstarlord@users.noreply.github.com>
2024-04-16 10:18:13 +0300

    Update pairv4.js

--- script/pairv4.js
+++ script/pairv4.js
@@ -1,82 +1,41 @@
+const moment = require("moment-timezone");
+const time = moment.tz("Asia/Manila").format("DD/MM/YYYY || HH:mm:s");
+const axios = require("axios");
+const request = require('request');
+const fs = require("fs");
+
 module.exports.config = {
-	name: "pair4",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "tdunguwu",
-	description: "",
-	commandCategory: "love",
-        hasPrefix:true,
-	cooldowns: 5,
-	dependencies: {
-        "axios": "",
-        "fs-extra": ""
-    }
-}
-module.exports.onLoad = async() => {
-    const { resolve } = global.nodemodule["path"];
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { downloadFile } = global.utils;
-    const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'pairing.jpg');
-    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.pinimg.com/736x/15/fa/9d/15fa9d71cdd07486bb6f728dae2fb264.jpg", path);
-}
+  name: "shoti",
+  version: "1.0.0",
+  cooldown: 10,
+  role: 0,
+  hasPrefix: true,
+  aliases: ['18+'],
+  description: "this command is 18+",
+  usage: "{pref}[name of cmd]",
+  credits: "Ainz"
+};
 
-async function makeImage({ one, two }) {
-    const fs = global.nodemodule["fs-extra"];
-    const path = global.nodemodule["path"];
-    const axios = global.nodemodule["axios"]; 
-    const jimp = global.nodemodule["jimp"];
-    const __root = path.resolve(__dirname, "cache", "canvas");
+module.exports.run = async function({ api, event }) {
+  let response = await axios.post('https://your-shoti-service-apis.onrender.com/api/v1/get', { apikey: "shoti-1hgn30msgapp542i0qg" });
+    const userInfo = response.data.data.user;
+    const videoInfo = response.data.data;
+    const title = videoInfo.title;
+    const durations = videoInfo.duration;
+    const region = videoInfo.region;
+    const username = userInfo.username;
+    const nickname = userInfo.nickname;
+  var file = fs.createWriteStream(__dirname + "/cache/shoti.mp4");
+  var rqs = request(encodeURI(response.data.data.url));
+  rqs.pipe(file);
+  file.on('finish', () => {
+    return api.sendMessage({
+      body: `[ 𝗔𝗨𝗧𝗢𝗠𝗔𝗧𝗘𝗗 𝗕𝗢𝗧 ]\n\n✨𝙷𝚎𝚛𝚎\'𝚜 𝚢𝚘𝚞𝚛 𝚜𝚑𝚘𝚝𝚒!\n𝘛𝘐𝘛𝘓𝘌: ${title}\n𝘕𝘐𝘊𝘒𝘕𝘈𝘔𝘌: ${nickname}\n𝘜𝘚𝘌𝘙𝘕𝘈𝘔𝘌: ${username}\n𝘋𝘜𝘙𝘈𝘛𝘐𝘖𝘕𝘚: ${durations}\n𝘋𝘈𝘛𝘌 𝘈𝘕𝘋 𝘛𝘐𝘔𝘌: ${time}`, 
+      attachment: fs.createReadStream(__dirname + '/cache/shoti.mp4')
+    }, event.threadID, event.messageID)
+  })
+  file.on('error', (err) => {
+      api.sendMessage(`Shoti Error: ${err}`, event.threadID, event.messageID);
+     })
 
-    let pairing_img = await jimp.read(__root + "/pairing.jpg");
-    let pathImg = __root + `/pairing_${one}_${two}.png`;
-    let avatarOne = __root + `/avt_${one}.png`;
-    let avatarTwo = __root + `/avt_${two}.png`;
-    
-    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    
-    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-    
-    let circleOne = await jimp.read(await circle(avatarOne));
-    let circleTwo = await jimp.read(await circle(avatarTwo));
-    pairing_img.composite(circleOne.resize(85, 85), 355, 100).composite(circleTwo.resize(75, 75), 250, 140);
-    
-    let raw = await pairing_img.getBufferAsync("image/png");
-    
-    fs.writeFileSync(pathImg, raw);
-    fs.unlinkSync(avatarOne);
-    fs.unlinkSync(avatarTwo);
-    
-    return pathImg;
-}
-async function circle(image) {
-    const jimp = require("jimp");
-    image = await jimp.read(image);
-    image.circle();
-    return await image.getBufferAsync("image/png");
-}
-module.exports.run = async function({ api, event, args, Users, Threads, Currencies }) {
-  const axios = require("axios");
-    const fs = require("fs-extra");
-    const { threadID, messageID, senderID } = event;
-    var tl = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
-        var tle = tl[Math.floor(Math.random() * tl.length)];
-        let dataa = await api.getUserInfo(event.senderID);
-        let namee = await dataa[event.senderID].name
-        let loz = await api.getThreadInfo(event.threadID);
-        var emoji = loz.participantIDs;
-        var id = emoji[Math.floor(Math.random() * emoji.length)];
-        let data = await api.getUserInfo(id);
-        let name = await data[id].name
-        var arraytag = [];
-                arraytag.push({id: event.senderID, tag: namee});
-                arraytag.push({id: id, tag: name});
-        
-        var sex = await data[id].gender;
-        var gender = sex == 2 ? "Male🧑" : sex == 1 ? "Female👩‍  " : "Tran Duc Bo";
-var one = senderID, two = id;
-    return makeImage({ one, two }).then(path => api.sendMessage({ body: `Congrats ${namee} has been paired with ${name}\nThe Match rate is: ${tle}`, mentions: arraytag, attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
-                                          }
+};
