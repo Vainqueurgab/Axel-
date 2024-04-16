7437e791921ee8736e5adf4f8d01ba5e0e994166
Thea / Aesther <154717468+Josephstarlord@users.noreply.github.com>
2024-04-16 06:46:04 +0300

    Update insult.js

--- script/insult.js
+++ script/insult.js
@@ -1,27 +1,45 @@
-const axios = require('axios');
 module.exports.config = {
-  name: "insult",
-  version: "1.0.0",
-  role: 0,
-  hasPrefix: true,
-  description: "Get a random insult.",
-  usage: "insult",
-  credits: "Developer",
-  cooldown: 0
+	name: "screenshot",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Mirai Team",
+	description: "Screenshot một trang web nào đó (NOT ALLOW NSFW PAGE)",
+	commandCategory: "Tiện Ích",
+	usages: "[url site]",
+  hasPrefix:true, 
+	cooldowns: 5,
+	dependencies: {
+        "fs-extra": "",
+        "path": "",
+        "url": ""
+    }
 };
-module.exports.run = async ({
-  api,
-  event
-}) => {
-  const {
-    threadID,
-    messageID
-  } = event;
-  try {
-    const response = await axios.get('https://evilinsult.com/generate_insult.php?lang=en&type=json');
-    const insult = response.data.insult;
-    api.sendMessage(`Here's a random insult for you: ${insult}`, threadID);
-  } catch (error) {
-    api.sendMessage("Sorry, I couldn't fetch an insult at the moment. Please try again later.", threadID, messageID);
-  }
-};
+
+module.exports.onLoad = async () => {
+    const { existsSync } = global.nodemodule["fs-extra"];
+    const { resolve } = global.nodemodule["path"];
+
+    const path = resolve(__dirname, "cache", "pornlist.txt");
+
+    if (!existsSync(path)) return await global.utils.downloadFile("https://raw.githubusercontent.com/blocklistproject/Lists/master/porn.txt", path);
+    else return;
+}
+
+module.exports.run = async ({ event, api, args, }) => {
+    const { readFileSync, createReadStream, unlinkSync } = global.nodemodule["fs-extra"];
+    const url = global.nodemodule["url"];
+
+    if (!global.moduleData.pornList) global.moduleData.pornList = readFileSync(__dirname + "/cache/pornlist.txt", "utf-8").split('\n').filter(site => site && !site.startsWith('#')).map(site => site.replace(/^(0.0.0.0 )/, ''));
+    const urlParsed = url.parse(args[0]);
+
+    if (global.moduleData.pornList.some(pornURL => urlParsed.host == pornURL)) return api.sendMessage("Trang web bạn nhập không an toàn!!(NSFW PAGE)", event.threadID, event.messageID);
+
+    try {
+        const path = __dirname + `/cache/${event.threadID}-${event.senderID}s.png`;
+        await global.utils.downloadFile(`https://image.thum.io/get/width/1920/crop/400/fullpage/noanimate/${args[0]}`, path);
+        api.sendMessage({ attachment: createReadStream(path) }, event.threadID, () => unlinkSync(path));
+    }
+    catch {
+        return api.sendMessage("Không tìm thấy url này, định dạng không đúng ?", event.threadID, event.messageID);
+    }
+}
