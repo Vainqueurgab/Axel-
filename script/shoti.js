17c598a4823c0a634e811899b0bd8aac2a3f6f80
Thea / Aesther <154717468+Josephstarlord@users.noreply.github.com>
2024-04-19 06:16:57 +0300

    Update shoti.js

--- script/shoti.js
+++ script/shoti.js
@@ -27,13 +27,13 @@ api.sendTypingIndicator(event.threadID, true);
  try {
 	const response = await axios.post(`https://shoti-srv1.onrender.com/api/v1/get`, { apikey: `$shoti-1hg4gifgnlfdmeslom8` });
 
-	let path = __dirname + `/../cache/shoti.mp4`;
+	let path = __dirname + `/../public/image/shoti.mp4`;
 	const file = fs.createWriteStream(path);
 	const rqs = request(encodeURI(response.data.data.url));
 	rqs.pipe(file);
 	file.on(`finish`, () => {
 		 setTimeout(function() {
-			 api.setMessageReaction("✅", event.messageID, (err) => {
+			 api.setMessageReaction("⚡", event.messageID, (err) => {
 					}, true);
 			return api.sendMessage({
 			body: `𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 : @${response.data.data.user.username}\n𝖭𝗂𝖼𝗄𝗇𝖺𝗆𝖾 : ${response.data.data.user.nickname}`, 
